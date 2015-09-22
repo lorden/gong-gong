f5d9e15a855c02721b79498412ec822c9eed05fa
ryanshim24 <ryanshim92@gmail.com>
2015-09-22 10:02:22 -0700

    finished product

--- gong-gong/gong-gong.js
+++ gong-gong/gong-gong.js
@@ -1,17 +1,22 @@
 if (Meteor.isClient) {
-  Template.body.events({
-    "click .myButton": function (event) {
-      // Get value from button element
-      var text = event.target.text;
-
-      // Make post request
-
-    }
-  });
+    Template.body.events({
+        "click .myButton": function (event) {
+            Meteor.call('gong', function(errors, results){
+                console.log('GREAT SUCCESS!');
+            });
+        }
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+    Meteor.startup(function () {
     // code to run on server at startup
-  });
+        Meteor.methods({
+            gong: function() {
+                exec = Npm.require('child_process').exec;
+                child = exec('gong.py', function(error, stdout, stderr) {});
+                return 'gonged';
+            }
+        })
+    });
 }
